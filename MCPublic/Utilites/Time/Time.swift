adb1500a832aa2784a7723229579a20e2b4c0f1e
JasonChengzi <3923240@qq.com>
2016-11-15 10:42:36 +0800

    update

--- MCPublic/Utilites/Time/Time.swift
+++ MCPublic/Utilites/Time/Time.swift
@@ -41,6 +41,40 @@ struct Time : CustomStringConvertible, Equatable {
             else if i == 2 { second = substrings[i].toInt() }
         }
     }
+    
+    func isAtAM() -> Bool {
+        if hour >= 0 && hour < 12 {
+            return true
+        }
+        return false
+    }
+    func isAtPM() -> Bool {
+        if hour >= 12 && hour < 24 {
+            return true
+        }
+        return false
+    }
+    
+    func isEarlier(than time : Time) -> Bool {
+        if hour < time.hour {
+            return true
+        } else if hour == time.hour {
+            if minute < time.minute {
+                return true
+            } else if minute == time.minute {
+                if second < time.second {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    func isLater(than time : Time) -> Bool {
+        if self == time {
+            return false
+        }
+        return !isEarlier(than: time)
+    }
 }
 func == (lhs: Time, rhs: Time) -> Bool {
     return lhs.hour == rhs.hour && lhs.minute == rhs.minute && lhs.second == rhs.second
